10a13b9c8ffae104f4f4308e173bcea878d39341
ernarl <arl.ernestas@gmail.com>
2023-10-16 17:13:25 +0300

    pjaustymas + assetai

---
+++ Assets/Scripts/SlicableObject.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlicableObject : MonoBehaviour
+{
+    public Material slicedMaterial;
+}
---
+++ Assets/Scripts/SliceObject.cs
@@ -0,0 +1,72 @@
+using EzySlice;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SliceObject : MonoBehaviour
+{
+    public Transform startSlicePoint;
+    public Transform endSlicePoint;
+    public VelocityEstimator velocityEstimator;
+    public LayerMask slicableLayer;
+
+    public float cutForce = 2000f;
+    private void FixedUpdate()
+    {
+        bool hasHit = Physics.Linecast(startSlicePoint.position, endSlicePoint.position, out RaycastHit hit, slicableLayer);
+        if(hasHit)
+        {
+            Debug.Log("Object Found");
+            GameObject target = hit.transform.gameObject;
+            Slice(target);
+        }
+    }
+
+    public void Slice(GameObject target)
+    {
+        Vector3 velocity = velocityEstimator.GetVelocityEstimate();
+        Vector3 planeNormal = Vector3.Cross(endSlicePoint.position - startSlicePoint.position, velocity);
+        planeNormal.Normalize();
+
+        Material crossSectionMaterial = target.GetComponent<SlicableObject>().slicedMaterial;
+        float mass = target.GetComponent<Rigidbody>().mass;
+
+        SlicedHull hull = target.Slice(endSlicePoint.position, planeNormal);
+
+        if(hull != null)
+        {
+            GameObject upperHull = hull.CreateUpperHull(target, crossSectionMaterial);
+            SetupSlicedComponent(upperHull, crossSectionMaterial, mass);
+
+            GameObject lowerHull = hull.CreateLowerHull(target, crossSectionMaterial);
+            SetupSlicedComponent(lowerHull, crossSectionMaterial, mass);
+
+            StartCoroutine(SetSlicable(upperHull, lowerHull, 0.35f));
+
+            Destroy(target);
+        }
+    }
+
+    public void SetupSlicedComponent(GameObject slicedObject, Material parentSlicedMaterial, float parentMass)
+    {
+        Rigidbody rb = slicedObject.AddComponent<Rigidbody>();
+        rb.mass = parentMass / 2 < 1 ? 1 : parentMass / 2;
+        SlicableObject slicableObject = slicedObject.AddComponent<SlicableObject>();
+        slicableObject.slicedMaterial = parentSlicedMaterial;
+        MeshCollider collider = slicedObject.AddComponent<MeshCollider>();
+        slicableObject.AddComponent<XRGrabInteractable>();
+        if(collider.sharedMesh.vertexCount >=4)
+            collider.convex = true;
+
+        rb.AddExplosionForce(cutForce, slicedObject.transform.position, 1);
+    }
+    IEnumerator SetSlicable(GameObject upperHull, GameObject lowerHull, float time)
+    {
+        yield return new WaitForSeconds(time);
+        upperHull.layer = 6;
+        lowerHull.layer = 6;
+    }
+}
